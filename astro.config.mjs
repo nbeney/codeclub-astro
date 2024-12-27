27b4bc83af4fc82f63e0aa1e91d016d912015e4a
nbeney <nbeney@gmail.com>
2024-12-27 11:34:46 +0000

    Try to fix routing issue with GHP.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://icantech.org',  // Replace with your GitHub Pages URL
+  base: '/codeclub-astro',  // Replace with your repository name
+});
