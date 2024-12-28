4d1529f0fc3309060e24198fc36194fa20cdd82e
nbeney <nbeney@gmail.com>
2024-12-28 12:56:08 +0000

    Move images to assets/images.

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,12 +1,12 @@
 function findJpgPath(pattern) {
-    const paths = import.meta.glob('/src/pages/**/*.jpg');
+    const paths = import.meta.glob('/src/assets/images/**/*.jpg');
     const key = Object.keys(paths).find((key) => key.includes(pattern));
     if (!key) throw new Error(`"${pattern}" does not exist in glob: "/src/pages/**/*.jpg"`);
     return paths[key]();
 }
 
 function findPngPath(pattern) {
-    const paths = import.meta.glob('/src/pages/**/*.png');
+    const paths = import.meta.glob('/src/assets/images/**/*.png');
     const key = Object.keys(paths).find((key) => key.includes(pattern));
     if (!key) throw new Error(`"${pattern}" does not exist in glob: "/src/pages/**/*.png"`);
     return paths[key]();
